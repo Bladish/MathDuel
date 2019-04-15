41cf855076e68278382917eb151b404a0849b668
Joakim Svensson <jkm.svensson@gmail.com>
2019-04-15 07:20:24 +0200

    Initial commit

---
+++ Assets/Scripts/HoldingObjects.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class HoldingObjects : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    public string inputFieldText;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("return"))
+        {
+            inputFieldText = inputField.text;
+            DontDestroyOnLoad(this.gameObject);
+            SceneManager.LoadScene("MathDuelPoC"); 
+        }
+    }
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public float startTime;
+    float time;
+
+
+    void Start()
+    {
+        timerText.text = startTime.ToString();
+        time = Time.time;
+    }
+
+    void FixedUpdate()
+    {
+        if (startTime > 0)
+        {
+            TimeCalcultion();
+            timerText.text = startTime.ToString();
+        }
+    }
+
+    void TimeCalcultion()
+    {
+        if (time < Time.time)
+        {
+            time = Time.time + 1;
+            startTime -= 1;
+        }
+    }
+}
